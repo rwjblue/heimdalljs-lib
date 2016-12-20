783a1e976ca6e19dd5a66fb2c9784858c4913b34
Chris Thoburn <cthoburn@linkedin.com>
2016-12-19 16:46:05 -0800

    use performance APIs if available

--- src/runtime/index.js
+++ src/runtime/index.js
@@ -23,6 +23,31 @@ function splitFirstColon(str) {
   return [first, rest];
 }
 
+const HAS_MEASURE_API = typeof performance !== 'undefined' && performance.mark && performance.measure;
+
+function markStart(label) {
+  if (HAS_MEASURE_API) {
+    performance.mark(`${label}:start`);
+  } else {
+    console.time(label);
+  }
+}
+
+function markEnd(label) {
+  if (HAS_MEASURE_API) {
+    performance.mark(`${label}:end`);
+    flushTimelineMeasurement(label);
+  } else {
+    console.timeEnd(label);
+  }
+}
+
+function flushTimelineMeasurement(label) {
+  if (HAS_MEASURE_API) {
+    performance.measure(label, `${label}:start`, `${label}:end`);
+  }
+}
+
 export default class Heimdall {
   constructor(session) {
     if (arguments.length < 1) {
@@ -79,7 +104,8 @@ export default class Heimdall {
       if (this._checkTimelineEnabledForNode(name)) {
         let label = `${name}--:${token}`;
         this._timelineInfo.timers[token] = label;
-        console.time(label);
+
+        markStart(label);
       }
     }
   }
@@ -89,7 +115,7 @@ export default class Heimdall {
       let label = this._timelineInfo.timers[token];
 
       if (label) {
-        console.timeEnd(label);
+        markEnd(label);
       }
     }
   }
